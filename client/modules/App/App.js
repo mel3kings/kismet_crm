23a98e829c5b90c879f17ad0d8013e057b046cec
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2018-07-25 19:21:20 +1000

    basic sidebar - to change css

--- client/modules/App/App.js
+++ client/modules/App/App.js
@@ -1,19 +1,17 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
-// Import Style
 import styles from './App.css';
 import Helmet from 'react-helmet';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Menu from './components/Menu/Menu';
 
-// Import Actions
 import { toggleAddPost } from './AppActions';
 import { switchLanguage } from '../../modules/Intl/IntlActions';
 
 let DevTools;
 if (process.env.NODE_ENV === 'development') {
-  // eslint-disable-next-line global-require
   DevTools = require('./components/DevTools').default;
 }
 
@@ -37,7 +35,7 @@ export class App extends Component {
         {this.state.isMounted && !window.devToolsExtension && process.env.NODE_ENV === 'development' && <DevTools />}
         <div>
           <Helmet
-            title="MERN Starter - Blog App"
+            title="Kismet CRM"
             titleTemplate="%s - Blog App"
             meta={[
               { charset: 'utf-8' },
@@ -56,6 +54,7 @@ export class App extends Component {
             intl={this.props.intl}
             toggleAddPost={this.toggleAddPostSection}
           />
+          <Menu/>
           <div className={styles.container}>
             {this.props.children}
           </div>
---
+++ client/modules/App/components/Menu/Menu.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './Menu.css'
+import {Link} from 'react-router';
+class Menu extends React.Component{
+  render(){
+    return   <div className={styles['sidebar']}>
+      <ul>
+        <li className={styles['sidebarlink']}>
+          <Link className={styles['sidebarlinktest']} to="/"> Customers</Link>
+        </li>
+
+        <li className={styles['sidebarlink']}>
+          <Link className={styles['sidebarlinktest']} to="/"> Emails</Link>
+        </li>
+      </ul>
+    </div>
+  }
+};
+
+export default Menu;
